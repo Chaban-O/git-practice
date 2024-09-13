3eef4beac5d5b0ec56e1750473cbc516e9d678b4
Your Name <you@example.com>
2024-09-13 20:47:22 +0300

    add private ip

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "allow_ssh_http_https" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Вкажіть вашу IP адресу
+    cidr_blocks = ["79.110.128.251/32"] # Вкажіть вашу IP адресу
   }
 
   ingress {
@@ -74,9 +74,13 @@ resource "aws_instance" "web" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y docker.io",
-      "sudo apt install -y python3-pip",
-      "sudo pip3 install docker-compose"
+      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+      "sudo curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
     ]
   }
 
